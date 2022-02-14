02b8b9dad1515954087fed0ea7e75e14ea2eb853
akrupin <aleksandr.krupin@leroymerlin.ru>
2022-02-14 12:03:58 +0300

    some fix

--- solution_test.go
+++ solution_test.go
@@ -1,15 +1,12 @@
 package solution
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGetMessage(t *testing.T) {
 	var s interface{} = GetMessage()
-	if _, ok := s.(string); ok {
-		fmt.Println("Variable is a string")
-	} else {
-		fmt.Println("Variable is not a string")
+	if _, ok := s.(string); !ok {
+		t.Error("Variable is not a String")
 	}
 }
