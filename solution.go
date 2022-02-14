b574a75d767aca6215ec4855023a4e9f4140f19a
akrupin <aleksandr.krupin@leroymerlin.ru>
2022-02-14 13:57:30 +0300

    update

--- solution.go
+++ solution.go
@@ -1,10 +1,13 @@
 package solution
 
 import (
+	"fmt"
+
 	"github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
 	helloWorldMap := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(helloWorldMap)
 	return helloWorldMap
 }
